5206bdf767d90e42beef19b65f27d582ee25140b
Giovanni Bruno <giovanni.bruno@soluzionifutura.it>
2023-03-07 14:25:35 +0100

    v0.4.0: cli accepts a url for input

--- bin/cli.js
+++ bin/cli.js
@@ -1,23 +1,34 @@
 #!/usr/bin/env node
+const axios = require("axios")
 const { generateSdk } = require("../dist")
 const argv = require('yargs-parser')(process.argv.slice(2))
 const { name, version } = require("../package.json")
 
-if (argv.h || argv.help) {
-  console.log(`${name} v${version}`)
-  console.log(usage)
-  process.exit(0)
-}
+void (async () => {
+  if (argv.h || argv.help) {
+    console.log(`${name} v${version}`)
+    console.log(usage)
+    process.exit(0)
+  }
+  
+  const usage = `sf-ts-sdk-gen [input] [output] --sdk-version [version] --sdk-name [name]`
+  const { _: [input, output], "sdk-version": sdkVersion, "sdk-name": sdkName } = argv
+  
+  if (!input || !output) {
+    console.error(usage)
+    process.exit(1)
+  }
 
-const usage = `sf-ts-sdk-gen [input] [output] --sdk-version [version] --sdk-name [name]`
-const { _: [input, output], "sdk-version": sdkVersion, "sdk-name": sdkName } = argv
+  console.log(`Parsing ${input} to ${output}`)
+  
+  let openapi
+  if (input.startsWith("http")) {
+    console.log(`Downloading api definition`)
+    const { data } = await axios.get(input)
+    openapi = data
+  }
 
-if (!input || !output) {
-  console.error(usage)
-  process.exit(1)
-}
-
-console.log(`Parsing ${input} to ${output}`)
-generateSdk({ openapi: input, outputFolder: output, sdkVersion, sdkName })
-  .then(() => console.log(`Generated ${output}`))
+  await generateSdk({ openapi: openapi || input, outputFolder: output, sdkVersion, sdkName })
+  console.log(`Generated ${output}`)
+})()
   .catch(console.error)
